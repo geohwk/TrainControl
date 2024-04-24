911bd73797a599aa30afe8c9022b091bedb75a1d
George Hawkins <geohwk@gmail.com>
2024-04-24 14:37:13 +0100

    Added controller and train clients. Added framework for controller client to dynamically get new clients

---
+++ ControllerClient/espControllerClient.ino
@@ -0,0 +1,187 @@
+#include <DRV8833.h>
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+#include <vector>
+
+// WiFi credentials
+const char * wifi_ssid = "TrainControl";
+const char * wifi_password = "ringousel";
+
+//MQTT Broker
+const char* mqtt_broker = "192.168.1.7";
+const int mqtt_port = 1883;
+const char* brokerUser = "";  // exp: myemail@mail.com
+const char* brokerPass = "";
+
+// Default MQTT topic
+String defaultTopic = "train1";
+
+// MQTT topics
+std::vector<String> topics;
+int currentTopicIndex = 0; // Index of the currently selected topic
+
+// GPIO pins for buttons
+const int button1Pin = D1;
+const int button2Pin = D2;
+const int button3Pin = D3;
+const int button4Pin = D4;
+const int button5Pin = D5;
+const int button6Pin = D6;
+
+// Potentiometer pin
+const int potentiometerPin = A0;
+
+// Initialize the WiFi and MQTT client
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Function prototypes
+void connectToWiFi();
+void reconnect();
+void button1Pressed();
+void button2Pressed();
+void button3Pressed();
+void button4Pressed();
+void button5Pressed();
+void button6Pressed();
+void callback();
+
+void setup() 
+{
+    Serial.begin(115200);
+
+    // Initialize buttons as inputs
+    pinMode(button1Pin, INPUT_PULLUP);
+    pinMode(button2Pin, INPUT_PULLUP);
+    pinMode(button3Pin, INPUT_PULLUP);
+    pinMode(button4Pin, INPUT_PULLUP);
+    pinMode(button5Pin, INPUT_PULLUP);
+    pinMode(button6Pin, INPUT_PULLUP);
+
+    connectToWiFi();
+    
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
+
+    while (!client.connected()) {
+      String client_id = "esp8266-client-";
+      client_id += String(WiFi.macAddress());
+      Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+      if (client.connect(client_id.c_str(), "", "")) {
+          Serial.println("Public emqx mqtt broker connected");
+      } else {
+          Serial.print("failed with state ");
+          Serial.print(client.state());
+          delay(2000);
+      }
+    }
+
+    client.subscribe("newClient");
+
+}
+
+void connectToWiFi() {
+  Serial.printf("Connecting to '%s'\n", wifi_ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_ssid, wifi_password);
+  if (WiFi.waitForConnectResult() == WL_CONNECTED) {
+    Serial.print("Connected. IP: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("Connection Failed!");
+  }
+}
+
+// Button action functions
+
+/*Button1: Adds another train topic to the list
+void button1Pressed() {
+  if (topics.size() < 10) { // Limit the number of topics to 10
+    topics.push_back("train" + String(topics.size() + 1));
+  }
+}
+
+//Button2: Remove currently selected topic from the list
+void button2Pressed() {
+  if (!topics.empty()) {
+    topics.erase(topics.begin() + currentTopicIndex);
+    if (currentTopicIndex >= topics.size()) {
+      currentTopicIndex = topics.size() - 1;
+    }
+  }
+}
+*/
+//Button3: Change selected topic
+void button3Pressed() {
+  currentTopicIndex++;
+  if (currentTopicIndex >= topics.size()) {
+    currentTopicIndex = 0;
+  }
+}
+
+void button4Pressed() {
+  // Action for Button 4
+}
+
+void button5Pressed() {
+  // Action for Button 5
+}
+
+void button6Pressed() {
+  // Action for Button 6
+}
+
+void readSpeed(){
+  // Read potentiometer value
+  int potValue = analogRead(potentiometerPin);
+  
+  int speedValue = (potValue/1023)*100;
+
+  // Publish potentiometer value to the currently selected speed subtopic
+  client.publish((topics[currentTopicIndex] + "/speed").c_str(), String(speedValue).c_str());
+}
+
+void callback(char *topic, byte *payload, unsigned int length){
+  Serial.print("New Client Added to network:");
+  String str;
+  for (int i = 0; i < length; i++) {
+      str += byteArray[i];
+  }
+  Serial.println(str);
+  topics.push_back(str);
+}
+
+void loop() 
+{
+  client.loop();
+
+  readSpeed();
+
+  // Check button presses and perform corresponding actions
+  if (digitalRead(button1Pin) == LOW) {
+    button1Pressed();
+    delay(100); // Debounce delay
+  }
+  if (digitalRead(button2Pin) == LOW) {
+    button2Pressed();
+    delay(100); // Debounce delay
+  }
+  if (digitalRead(button3Pin) == LOW) {
+    button3Pressed();
+    delay(100); // Debounce delay
+  }
+  if (digitalRead(button4Pin) == LOW) {
+    button4Pressed();
+    delay(100); // Debounce delay
+  }
+  if (digitalRead(button5Pin) == LOW) {
+    button5Pressed();
+    delay(100); // Debounce delay
+  }
+  if (digitalRead(button6Pin) == LOW) {
+    button6Pressed();
+    delay(100); // Debounce delay
+  }
+
+}
---
+++ TrainClient/espTrainClient.ino
@@ -0,0 +1,139 @@
+#include <DRV8833.h>
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+#include <vector>
+
+DRV8833 driver = DRV8833();
+
+// WiFi credentials
+const char * wifi_ssid = "TrainControl";
+const char * wifi_password = "ringousel";
+
+
+//const int inputA1 = 5, inputA2 = 4;//d4, d5
+const int inputA1 = 2, inputA2 = 14;
+
+const char* brokerUser = "";  // exp: myemail@mail.com
+const char* brokerPass = "";
+const char* topic = "train1/trainSpeed";
+const char* mqtt_broker = "192.168.1.7";
+const int mqtt_port = 1883;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() 
+{
+    Serial.begin(115200);
+
+
+    pinMode(2,OUTPUT);
+    pinMode(14,OUTPUT);
+    //pinMode(5,OUTPUT);
+    //pinMode(4,OUTPUT);
+    pinMode(15,OUTPUT);
+    digitalWrite(15, LOW);
+    connectToWiFi();
+    
+
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
+
+    while (!client.connected()) {
+      String client_id = "esp8266-client-";
+      client_id += String(WiFi.macAddress());
+      Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+      if (client.connect(client_id.c_str(), "", "")) {
+          Serial.println("Public emqx mqtt broker connected");
+      } else {
+          Serial.print("failed with state ");
+          Serial.print(client.state());
+          delay(2000);
+      }
+    }
+    // publish and subscribe
+    client.publish(topic, "hello emqx");
+    client.subscribe(topic);
+    client.subscribe("train1/lights");
+    
+    // Attach the motors to the input pins:
+    driver.attachMotorA(inputA1, inputA2);
+    
+
+}
+
+void connectToWiFi() {
+  Serial.printf("Connecting to '%s'\n", wifi_ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_ssid, wifi_password);
+  if (WiFi.waitForConnectResult() == WL_CONNECTED) {
+    Serial.print("Connected. IP: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("Connection Failed!");
+  }
+}
+
+void callback(char *topic, byte *payload, unsigned int length) 
+{
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  String pwmValue_str = "";
+  for (int i = 0; i < length; i++) 
+  {
+      Serial.print((char) payload[i]);
+      pwmValue_str = pwmValue_str += payload[i];
+  }
+  Serial.println();
+  Serial.println("-----------------------");
+
+  payload[length] = '\0';
+  int pwmVal = atoi((char *)payload);
+
+  String topicString = String(topic);
+
+  if(topicString == "train1/trainSpeed")
+  {
+    controlSpeed(pwmVal);
+  }
+  else if(topicString == "train1/lights")
+  {
+    controlLights(pwmVal);
+  }
+}
+
+
+
+void controlSpeed(int pwmVal)
+{
+   int output = int(float((float(float(pwmVal)/100)*1023)));
+   Serial.println("control Speed function");
+   if(output < 0)
+   {
+      output = abs(output);
+      driver.motorAReverse(output);
+   }
+   else
+   {
+      driver.motorAForward(output);
+   }
+}
+
+void controlLights(int value)
+{
+  Serial.println("control Lights function");
+   if(value == 1)
+   {
+      digitalWrite(15, HIGH); #d8
+   }
+   else if(value == 0)
+   {
+      digitalWrite(15, LOW);
+   }
+}
+
+void loop() 
+{
+  client.loop();
+}
