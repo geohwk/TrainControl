eeaebfdbfa5be134c9a5d5458c8a245a28f186e6
geohwk <geohwk@gmail.com>
2024-05-04 20:10:24 +0100

    Screen added

--- ControllerClient/espControllerClient.ino
+++ ControllerClient/espControllerClient.ino
@@ -2,6 +2,47 @@
 #include <ESP8266WiFi.h>
 #include <vector>
 #include <string>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//Screen Setup
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b00000000, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b11100000,
+  0b11110011, 0b11100000,
+  0b11111110, 0b11111000,
+  0b01111110, 0b11111111,
+  0b00110011, 0b10011111,
+  0b00011111, 0b11111100,
+  0b00001101, 0b01110000,
+  0b00011011, 0b10100000,
+  0b00111111, 0b11100000,
+  0b00111111, 0b11110000,
+  0b01111100, 0b11110000,
+  0b01110000, 0b01110000,
+  0b00000000, 0b00110000 };
+
 
 // WiFi credentials
 char wifi_ssid[] = "TrainControl";
@@ -40,6 +81,22 @@ float currentSpeed = 0;
 void setup() 
 {
     Serial.begin(115200);
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+      Serial.println(F("SSD1306 allocation failed"));
+      for(;;); // Don't proceed, loop forever
+    }
+
+    // Clear the buffer
+    display.clearDisplay();
+
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);        // Draw white text
+    display.setCursor(0,0);             // Start at top-left corner
+    display.println(F("No Connected Trains"));
+    display.display();
+    delay(2000)
 
     // Connect to Pi Access Point
     connectToWiFi();
@@ -95,6 +152,10 @@ void removeEntry() {
     return;
   }
   Serial.println("Removed Client from Controller...");
+  
+  display.clearDisplay();
+  display.println(F(topics[currentTopicIndex]));
+  display.display();
 }
 
 void swapEntry() {
@@ -109,7 +170,11 @@ void swapEntry() {
     Serial.println("Less than 2 clients connected, cannot go to next entry...");
     return;
   }
-  
+
+  display.clearDisplay();
+  display.println(F(topics[currentTopicIndex]));
+  display.display();
+
 }
 
 void lights1On(){
